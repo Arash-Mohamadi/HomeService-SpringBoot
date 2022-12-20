3e092db612aba0511113f7690f511224c1beea91
Arashap4 <arashmohamadi.developer@gmail.com>
2022-12-20 19:24:41 +0330

    implementation suggestion service

--- src/main/java/com/example/homeservicespringboot/service/SuggestionService.java
+++ src/main/java/com/example/homeservicespringboot/service/SuggestionService.java
@@ -2,6 +2,14 @@ package com.example.homeservicespringboot.service;
 
 import com.example.homeservicespringboot.entity.capability.Suggestion;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SuggestionService {
     boolean createSuggestion(Suggestion suggestion);
+
+    List<Suggestion> findSuggestionWithPrice(Long id);
+    List<Suggestion> findSuggestionWithSpecialistScore(Long id);
+
+    Optional<Suggestion> findSuggestionWithId(Long suggestionId);
 }
--- src/main/java/com/example/homeservicespringboot/service/impl/SuggestionServiceImpl.java
+++ src/main/java/com/example/homeservicespringboot/service/impl/SuggestionServiceImpl.java
@@ -1,19 +1,51 @@
 package com.example.homeservicespringboot.service.impl;
 
 import com.example.homeservicespringboot.entity.capability.Suggestion;
-import com.example.homeservicespringboot.repository.SubServiceRepository;
+import com.example.homeservicespringboot.repository.SuggestionRepository;
 import com.example.homeservicespringboot.service.SuggestionService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
+@Transactional
 public class SuggestionServiceImpl implements SuggestionService {
-    private SubServiceRepository subServiceRepository ;
-    // constructor
+    private SuggestionRepository suggestionRepository;
 
+    // constructor
+    public SuggestionServiceImpl(SuggestionRepository suggestionRepository) {
+        this.suggestionRepository = suggestionRepository;
+    }
 
     @Override
     public boolean createSuggestion(Suggestion suggestion) {
-        return false;
+        try {
+            suggestionRepository.save(suggestion);
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public List<Suggestion> findSuggestionWithPrice(Long id) {
+
+        return suggestionRepository.findAllSuggestionOneOrderByPrice(id);
+
+    }
+
+    @Override
+    public List<Suggestion> findSuggestionWithSpecialistScore(Long id) {
+
+        return suggestionRepository.findAllSuggestionOneOrderBySpecialistScore(id);
+    }
+
+    @Override
+    public Optional<Suggestion> findSuggestionWithId(Long suggestionId) {
+        return suggestionRepository.findById(suggestionId);
     }
 
 }
