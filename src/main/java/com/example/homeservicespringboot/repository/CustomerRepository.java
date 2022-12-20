01b4cecc9f91361af39618f32403ac77c64e16b7
Arashap4 <arashmohamadi.developer@gmail.com>
2022-12-20 19:20:48 +0330

    create customer repository

--- src/main/java/com/example/homeservicespringboot/repository/CustomerRepository.java
+++ src/main/java/com/example/homeservicespringboot/repository/CustomerRepository.java
@@ -4,8 +4,12 @@ import com.example.homeservicespringboot.entity.users.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
+    Optional<Customer> findCustomerByUsername(String username);
 
 }
